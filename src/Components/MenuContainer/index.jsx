b5b46de8b20e0321f521ca454839132d76a0a230
NadCarvalho <carvalho.nadiacaroline@gmail.com>
2021-09-07 20:21:53 -0300

    Menu Container finalizado

--- src/Components/MenuContainer/index.jsx
+++ src/Components/MenuContainer/index.jsx
@@ -1,5 +1,17 @@
-const MenuContainer = () => {
-    return()
+import Product from '../Product'
+
+const MenuContainer = ({ productsList, handleAdd }) => {
+  return (
+    <ul>
+      {productsList.map(product =>
+        <li>
+          <Product item={product}
+            handleClick={handleAdd} />
+        </li>
+      )}
+
+    </ul>
+  )
 }
 
 export default MenuContainer;
